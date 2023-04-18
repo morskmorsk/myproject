b16847e3442595302d32ec3e5c0552143109381e
morskmorsk <khalidfour@yahoo.com>
2023-04-17 22:59:23 -0600

    create shopping cart create django built-in authentication

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 
