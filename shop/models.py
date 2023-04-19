dce0bafa14b55a55f41c748f31592b6841811436
morskmorsk <khalidfour@yahoo.com>
2023-04-19 09:25:41 -0600

    add WorkOrderItem

--- shop/models.py
+++ shop/models.py
@@ -22,3 +22,15 @@ class ShoppingCartItem(models.Model):
 
     def get_total_price(self):
         return self.product.price * self.quantity
+
+
+class WorkOrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.quantity} of {self.product.name}'
+
+    def get_total_price(self):
+        return self.product.price * self.quantity
